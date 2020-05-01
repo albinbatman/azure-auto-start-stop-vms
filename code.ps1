fdcfbb4f9ab55e38078f127cacc3150e72e37883
100 Procent <gitlab@100procent.com>
2020-05-01 16:35:44 +0200

    � Initial commit

---
+++ code.ps1
@@ -0,0 +1,118 @@
+﻿Param(
+     # Parameters that contains the input value provided by the user from Azure Automation.
+
+     # Used by us to know which virtual machine to start/stop.
+     [string]$VmName,
+     # Used by us to know which virtual machine to start/stop within a resource group.
+     [string]$ResourceGroupName,
+     # Used by us to know which what to do (ie to start or to stop)
+     [ValidateSet("Start", "Stop")]
+     [string]$VmAction
+)
+ 
+    # Being able to run it through Azure Automation.
+    $connectionName = "AzureRunAsConnection"
+    try
+    {
+        # Get the connection "AzureRunAsConnection"
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+        "Logging in to Azure..."
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint > $null
+    } catch {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+
+# Get current date
+$UTCTime = (Get-Date).ToUniversalTime()
+$TZ = [System.TimeZoneInfo]::FindSystemTimeZoneById("Central Europe Standard Time")
+$LocalTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($UTCTime, $TZ)
+$day = $LocalTime.DayOfWeek
+
+# Validate whether this is running on a weekend
+# if you wish to use different days you can use any of the following:
+# Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+if ($day -eq 'Saturday' -or $day -eq 'Sunday')
+{
+    Write-Output ("It is $($day). Cannot use a runbook to start VMs on a weekend.")
+    Exit
+} else {
+    Write-Output ("✔️ It is $($day). Continuing...")
+
+    # Start 
+    if ($VmAction -eq "Start") {
+        # If input is wildcard, then start all virtual machines within resource group
+        if($VmName -eq "*") {
+            $vms = Get-AzureRmVM -ResourceGroupName $ResourceGroupName | Select Name
+            Foreach ( $vm in $vms )
+            {
+                Write-Output ("🔌 Starting $($vm.Name) on resource group $($ResourceGroupName)...")
+                Start-AzureRmVM -Name $vm.Name -ResourceGroupName $ResourceGroupName > $null
+                $state = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $vm.Name -Status > $null
+
+                if($state.Statuses[1].Code -eq "PowerState/running")
+                {
+                    Write-Output ("✔️ Successfully started $($vm.Name), it is now running.")
+                } else {
+                    Write-Output ("❌ Unable to start $($vm.Name), current status: $($state.Statuses[1].Code).")
+                } 
+            }
+        } else {
+        # If not, just start the specific one
+            Write-Output ("🔌 Starting $($VmName) on resource group $($ResourceGroupName)...")
+            Start-AzureRmVM -Name $VmName -ResourceGroupName $ResourceGroupName
+            $state = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VmName -Status > $null
+
+            if($state.Statuses[1].Code -eq "PowerState/running")
+            {
+                Write-Output ("✔️ Successfully started $($VmName), it is now running.")
+            } else {
+                Write-Output ("❌ Unable to start $($VmName), current status: $($state.Statuses[1].Code).")
+            }
+        }
+    }
+ 
+    # Stop
+    if ($VmAction -eq "Stop") {
+        # If input is wildcard, then stop all virtual machines within resource group
+        if($VmName -eq "*") {
+            $vms = Get-AzureRmVM -ResourceGroupName $ResourceGroupName | Select Name
+            Foreach ( $vm in $vms )
+            {
+                Write-Output ("🔌 Stopping $($vm.Name) on resource group $($ResourceGroupName)...")
+                Stop-AzureRmVM -Name $vm.Name -ResourceGroupName $ResourceGroupName -Force
+                $state = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $vm.Name -Status > $null
+
+                if($state.Statuses[1].Code -eq "PowerState/deallocated")
+                {
+                    Write-Output ("✔️ Successfully stopped $($vm.Name), it is now deallocated.")
+                } else {
+                    Write-Output ("❌ Unable to stop $($vm.Name), current status: $($state.Statuses[1].Code).")
+                } 
+            }
+        } else {
+        # If not, just stop the specific one
+            Write-Output ("🔌 Stopping $($VmName) on resource group $($ResourceGroupName)...")
+            Stop-AzureRmVM -Name $VmName -ResourceGroupName $ResourceGroupName -Force
+            $state = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VmName -Status > $null
+
+            if($state.Statuses[1].Code -eq "PowerState/deallocated")
+            {
+                Write-Output ("✔️ Successfully stopped $($VmName), it is now deallocated.")
+            } else {
+                Write-Output ("❌ Unable to stop $($VmName), current status: $($state.Statuses[1].Code).")
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,47 @@
+![](https://i.imgur.com/N6GqEC1.png)
+
+# Azure: Auto start/stop VM
+
+This template allows you to quickly set up an automatic schedule that starts and stops your virtual machine(s) in Azure, based solely on your own schedule and adaptation of the code. This code is based on starting and stopping the virtual machines on weekdays (Mon-Fri) and exclude the weekends (Sat-Sun).
+
+## ⚠️ Before you continuing reading...
+Please review [Pricing - Automation | Microsoft Azure](https://azure.microsoft.com/en-us/pricing/details/automation/) because this script expects you to use Automation in Azure and is also solely developed for this purpose.
+
+If you are looking for a different solution, please reconsider the following ones:
+
+### Azure-CLI (Bash)
+The Azure CLI allows you to create and manage your Azure resources on macOS, Linux, and Windows.
+* [Start or Stop all VMs of a Resource Group in Azure - attosol.com](https://www.attosol.com/start-or-stop-all-vms-of-a-resource-group-in-azure/)
+## ⚙️ Installation
+
+1. Download (or copy + paste) *code.ps1* to your local computer.
+2. Go to [Automation Accounts - Microsoft Azure](https://portal.azure.com/#blade/HubsExtension/BrowseResource/resourceType/Microsoft.Automation%2FAutomationAccounts) and create a new account or use an existing one.
+3. Below **Process Automation**, press **Runbooks**.
+   1. Press **Create a runbook**.
+   2. Give it a name (ie. **StartStopVM-ExcludingWeekends**) and select *PowerShell* under **Runbook type**.
+   3. Once inside your newly created runbook, press the **Edit**-button on the top menu & then paste the code in there then press **Save** & **Publish**.
+4. Go to **Schedules** to create a new schedule. We need one to start the VM and one to stop the VM.
+   1. Press **Schedule** and then **Create a new schedule**.
+      1. Give it a name (ie. **Start YourVirtualMachineName**) and an appropriate description.
+      2. Make it start from today or tomorrow and specify the time (ie. **7 AM** to start) and make it recurring on every week and select Mon-Fri (to exclude weekends).
+      3. Press **Parameters and run settings** and specify the details. (VMACTION: **Start** = starts the VM, **Stop** = stops the VM)
+   2. Create a new **schedule** (as you did in 4.i)
+      1. Give it a name (ie. **Stop YourVirtualMachineName**) and an appropriate description.
+      2. Make it start from today or tomorrow and specify the time (ie. **8 PM** to start) and make it recurring on every week and select Mon-Fri (to exclude weekends).
+      3. Press **Parameters and run settings** and specify the details. (VMACTION: **Start** = starts the VM, **Stop** = stops the VM)
+
+## 🏷️ Features
+* Automatically starts or stops all or one specific virtual machine within a resource group
+* After each start/stop, checks status of each virtual machine to ensure it is in the proper state
+* Unicode characters to visually represent all states
+
+## 💡 Ideas to implement
+* Allow for more specified virtual machines to be entered in **VmName** to split into an array so the script can start/stop multiple machines instead of either *all* or *one*
+
+#### 📚 Good-to-know
+A collection of good-to-know things about this script.
+
+##### 🔌 Starting or stopping all VMs in a resource group
+To start or stop multiple virtual machines within a specified resource group, you may do the following:
+1. When scheduling in your automation account you may specify the **VMNAME** with an asterisk (*) in order to select all the virtual machines within the specified resource group.
+
